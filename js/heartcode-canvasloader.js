9b1e0166949989a924598d00313d3d034920f83f
Robert Pataki <heartcode@robertpataki.com>
2011-10-17 19:30:49 +0100

    Code optimazition
    - Variable and function names are shortened.
    - Minified file updated.
    - Documentation updated.

--- js/heartcode-canvasloader.js
+++ js/heartcode-canvasloader.js
@@ -43,8 +43,8 @@
 	* @param {String} id The id of the placeholder div
 	**/
 	var CanvasLoader = function (id) {
-		this.initialize(id);
-	}, p = CanvasLoader.prototype, engine, engines = ["canvas", "vml"], shapes = ["oval", "spiral", "square", "rect", "roundRect"], colorReg = /^\#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/, ie8 = navigator.appVersion.indexOf("MSIE") !== -1 && parseFloat(navigator.appVersion.split("MSIE")[1]) === 8 ? true : false, canvasSupport = !!document.createElement('canvas').getContext,
+		this.init(id);
+	}, p = CanvasLoader.prototype, engine, engines = ["canvas", "vml"], shapes = ["oval", "spiral", "square", "rect", "roundRect"], cRX = /^\#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/, ie8 = navigator.appVersion.indexOf("MSIE") !== -1 && parseFloat(navigator.appVersion.split("MSIE")[1]) === 8 ? true : false, canSup = !!document.createElement('canvas').getContext,
 	/**
 	* Creates a new element with the tag and applies the passed properties on it
 	* @method addEl
@@ -87,15 +87,15 @@
 			return el;
 		},
 	/**
-	* Prepares the cache canvas for drawing
-	* @method transformContext
+	* Transforms the cache canvas before drawing
+	* @method transCon
 	* @protected
 	* @param	x {Object} The canvas context to be transformed
 	* @param	x {Number} x translation
 	* @param	y {Number} y translation
 	* @param	r {Number} Rotation radians
 	*/
-		transformContext = function(c, x, y, r) {
+		transCon = function(c, x, y, r) {
 			c.save();
 			c.translate(x, y);
 			c.rotate(r);
@@ -104,38 +104,38 @@
 		};
 	/** 
 	* Initialization method
-	* @method initialize
+	* @method init
 	* @protected
-	* @param parentId (String) The id of the placeholder div, where the loader will be nested into
-	* @param loaderId (String) The id of loader
+	* @param pId (String) The id of the placeholder div, where the loader will be nested into
+	* @param lId (String) The id of loader
 	*/
-	p.initialize = function (parentId, loaderId) {
+	p.init = function (pId, lId) {
 		/*
 		* Find the containing div by id
 		* If the container element cannot be found we use the document body itself
 		*/
 		try {
 			// Look for the parent element
-			if (document.getElementById(parentId) !== undefined) {
-				this.mother = document.getElementById(parentId);
+			if (document.getElementById(pId) !== undefined) {
+				this.mum = document.getElementById(pId);
 			} else {
-				this.mother = document.body;
+				this.mum = document.body;
 			}
 		} catch (error) {
-			this.mother = document.body;
+			this.mum = document.body;
 		}
 		// Creates the parent div of the loader instance
-		loaderId = typeof (loaderId) !== "undefined" ? loaderId : "canvasLoader";
-		this.cont = addEl("div", this.mother, {id: loaderId});
-		if (canvasSupport) {
+		lId = typeof (lId) !== "undefined" ? lId : "canvasLoader";
+		this.cont = addEl("div", this.mum, {id: lId});
+		if (canSup) {
 		// For browsers with Canvas support...
 			engine = engines[0];
 			// Create the canvas element
-			this.canvas = addEl("canvas", this.cont);
-			this.context = this.canvas.getContext("2d");
+			this.can = addEl("canvas", this.cont);
+			this.con = this.can.getContext("2d");
 			// Create the cache canvas element
-			this.cacheCanvas = setCSS(addEl("canvas", this.cont), { display: "none" });
-			this.cacheContext = this.cacheCanvas.getContext("2d");
+			this.cCan = setCSS(addEl("canvas", this.cont), { display: "none" });
+			this.cCon = this.cCan.getContext("2d");
 		} else {
 		// For browsers without Canvas support...
 			engine = engines[1];
@@ -161,39 +161,39 @@
 // Property declarations
 	/**
 	* The div we place the canvas object into
-	* @property container
+	* @property cont
 	* @protected
 	* @type Object
 	**/
-	p.container = {};
+	p.cont = {};
 	/**
 	* The div we draw the shapes into
-	* @property canvas
+	* @property can
 	* @protected
 	* @type Object
 	**/
-	p.canvas = {};
+	p.can = {};
 	/**
 	* The canvas context
-	* @property context
+	* @property con
 	* @protected
 	* @type Object
 	**/
-	p.context = {};
+	p.con = {};
 	/**
 	* The canvas we use for caching
-	* @property cacheCanvas
+	* @property cCan
 	* @protected
 	* @type Object
 	**/
-	p.cacheCanvas = {};
+	p.cCan = {};
 	/**
 	* The context of the cache canvas
-	* @property cacheContext
+	* @property cCon
 	* @protected
 	* @type Object
 	**/
-	p.cacheContext = {};
+	p.cCon = {};
 	/**
 	* Tells if the canvas and its context is ready
 	* @property ready
@@ -239,11 +239,11 @@
 	p.getDiameter = function () { return this.diameter; };
 	/**
 	* The color of the loader shapes in RGB
-	* @property colorRGB
+	* @property cRGB
 	* @protected
 	* @type Object
 	**/
-	p.colorRGB = {};
+	p.cRGB = {};
 	/**
 	* The color of the loader shapes in HEX
 	* @property color
@@ -258,7 +258,7 @@
 	* @public
 	* @param color {String} The default value is '#000000'
 	**/
-	p.setColor = function (color) { this.color = colorReg.test(color) ? color : "#000000"; this.colorRGB = this.getRGB(this.color); this.redraw(); };
+	p.setColor = function (color) { this.color = cRX.test(color) ? color : "#000000"; this.cRGB = this.getRGB(this.color); this.redraw(); };
 	/**
 	* Returns the loader color in a hexadecimal form
 	* @method getColor
@@ -401,9 +401,9 @@
 	* @protected
 	* @param color {String} The HEX color value to be converted to RGB
 	*/
-	p.getRGB = function (color) {
-		color = color.charAt(0) === "#" ? color.substring(1, 7) : color;
-		return {r: parseInt(color.substring(0, 2), 16), g: parseInt(color.substring(2, 4), 16), b: parseInt(color.substring(4, 6), 16) };
+	p.getRGB = function (c) {
+		c = c.charAt(0) === "#" ? c.substring(1, 7) : c;
+		return {r: parseInt(c.substring(0, 2), 16), g: parseInt(c.substring(2, 4), 16), b: parseInt(c.substring(4, 6), 16) };
 	};
 	/**
 	* Draw the shapes on the canvas
@@ -411,63 +411,63 @@
 	* @protected
 	*/
 	p.draw = function () {
-		var i = 0, size, w, h, x, y, angle, radians, radius, animBits = Math.round(this.density * this.range), bitMod, minBitMod = 0, s, g, sh, f, d = 1000, arc = 0;
+		var i = 0, size, w, h, x, y, ang, rads, rad, de = this.density, animBits = Math.round(de * this.range), bitMod, minBitMod = 0, s, g, sh, f, d = 1000, arc = 0, c = this.cCon, di = this.diameter;
 		if (engine === engines[0]) {
-			this.cacheContext.clearRect(0, 0, this.cacheCanvas.width, this.cacheCanvas.height);
-			setAttr(this.canvas, {width: this.diameter, height: this.diameter});
-			setAttr(this.cacheCanvas, {width: this.diameter, height: this.diameter});
-			while (i < this.density) {
+			c.clearRect(0, 0, 10000, 10000);
+			setAttr(this.can, {width: di, height: di});
+			setAttr(this.cCan, {width: di, height: di});
+			while (i < de) {
 				bitMod = i <= animBits ? 1 - ((1 - minBitMod) / animBits * i) : bitMod = minBitMod;
-				angle = 360 - 360 / this.density * i;
-				radians = angle / 180 * Math.PI;
-				this.cacheContext.fillStyle = "rgba(" + this.colorRGB.r + "," + this.colorRGB.g + "," + this.colorRGB.b + "," + bitMod.toString() + ")";
+				ang = 360 - 360 / de * i;
+				rads = ang / 180 * Math.PI;
+				c.fillStyle = "rgba(" + this.cRGB.r + "," + this.cRGB.g + "," + this.cRGB.b + "," + bitMod.toString() + ")";
 				switch (this.shape) {
 				case shapes[0]:
 				case shapes[1]:
-					size = this.diameter * 0.07;
-					x = this.canvas.width * 0.5 + Math.cos(radians) * (this.diameter * 0.5 - size) - this.canvas.width * 0.5;
-					y = this.canvas.height * 0.5 + Math.sin(radians) * (this.diameter * 0.5 - size) - this.canvas.height * 0.5;
-					this.cacheContext.beginPath();
-					if (this.shape === shapes[1]) { this.cacheContext.arc(this.diameter * 0.5 + x, this.diameter * 0.5 + y, size * bitMod, 0, Math.PI * 2, false); } else { this.cacheContext.arc(this.diameter * 0.5 + x, this.diameter * 0.5 + y, size, 0, Math.PI * 2, false); }
+					size = di * 0.07;
+					x = di * 0.5 + Math.cos(rads) * (di * 0.5 - size) - di * 0.5;
+					y = di * 0.5 + Math.sin(rads) * (di * 0.5 - size) - di * 0.5;
+					c.beginPath();
+					if (this.shape === shapes[1]) { c.arc(di * 0.5 + x, di * 0.5 + y, size * bitMod, 0, Math.PI * 2, false); } else { c.arc(di * 0.5 + x, di * 0.5 + y, size, 0, Math.PI * 2, false); }
 					break;
 				case shapes[2]:
-					size = this.diameter * 0.12;
-					x = Math.cos(radians) * (this.cacheCanvas.width * 0.5 - size) + this.cacheCanvas.width * 0.5;
-					y = Math.sin(radians) * (this.cacheCanvas.width * 0.5 - size) + this.cacheCanvas.height * 0.5;
-					transformContext(this.cacheContext, x, y, radians);
-					this.cacheContext.fillRect(x, y - size * 0.5, size, size);
+					size = di * 0.12;
+					x = Math.cos(rads) * (di * 0.5 - size) + di * 0.5;
+					y = Math.sin(rads) * (di * 0.5 - size) + di * 0.5;
+					transCon(c, x, y, rads);
+					c.fillRect(x, y - size * 0.5, size, size);
 					break;
 				case shapes[3]:
 				case shapes[4]:
-					w = this.diameter * 0.3;
+					w = di * 0.3;
 					h = w * 0.27;
-					x = Math.cos(radians) * (h + (this.cacheCanvas.height - h) * 0.13) + this.cacheCanvas.width * 0.5;
-					y = Math.sin(radians) * (h + (this.cacheCanvas.height - h) * 0.13) + this.cacheCanvas.height * 0.5;
-					transformContext(this.cacheContext, x, y, radians);
+					x = Math.cos(rads) * (h + (di - h) * 0.13) + di * 0.5;
+					y = Math.sin(rads) * (h + (di - h) * 0.13) + di * 0.5;
+					transCon(c, x, y, rads);
 					if(this.shape === shapes[3]) {
-						this.cacheContext.fillRect(x, y - h * 0.5, w, h);
+						c.fillRect(x, y - h * 0.5, w, h);
 					} else {
-						radius = h * 0.7;
-						this.cacheContext.moveTo(x + radius, y - h * 0.5);
-						this.cacheContext.lineTo(x + w - radius, y - h * 0.5);
-						this.cacheContext.quadraticCurveTo(x + w, y - h * 0.5, x + w, y - h * 0.5 + radius);
-						this.cacheContext.lineTo(x + w, y - h * 0.5 + h - radius);
-						this.cacheContext.quadraticCurveTo(x + w, y - h * 0.5 + h, x + w - radius, y - h * 0.5 + h);
-						this.cacheContext.lineTo(x + radius, y - h * 0.5 + h);
-						this.cacheContext.quadraticCurveTo(x, y - h * 0.5 + h, x, y - h * 0.5 + h - radius);
-						this.cacheContext.lineTo(x, y - h * 0.5 + radius);
-						this.cacheContext.quadraticCurveTo(x, y - h * 0.5, x + radius, y - h * 0.5);
+						rad = h * 0.7;
+						c.moveTo(x + rad, y - h * 0.5);
+						c.lineTo(x + w - rad, y - h * 0.5);
+						c.quadraticCurveTo(x + w, y - h * 0.5, x + w, y - h * 0.5 + rad);
+						c.lineTo(x + w, y - h * 0.5 + h - rad);
+						c.quadraticCurveTo(x + w, y - h * 0.5 + h, x + w - rad, y - h * 0.5 + h);
+						c.lineTo(x + rad, y - h * 0.5 + h);
+						c.quadraticCurveTo(x, y - h * 0.5 + h, x, y - h * 0.5 + h - rad);
+						c.lineTo(x, y - h * 0.5 + rad);
+						c.quadraticCurveTo(x, y - h * 0.5, x + rad, y - h * 0.5);
 					}
 					break;
 				}
-				this.cacheContext.closePath();
-				this.cacheContext.fill();
-				this.cacheContext.restore();
+				c.closePath();
+				c.fill();
+				c.restore();
 				++i;
 			}
 		} else {
-			setCSS(this.cont, {width: this.diameter, height: this.diameter});
-			setCSS(this.vml, {width: this.diameter, height: this.diameter, border: "1px solid #ff0000"});
+			setCSS(this.cont, {width: di, height: di});
+			setCSS(this.vml, {width: di, height: di});
 			switch (this.shape) {
 			case shapes[0]:
 			case shapes[1]:
@@ -487,9 +487,9 @@
 			w = h = size;
 			x = d * 0.5 - h;
 			y = -h * 0.5;		
-			while (i < this.density) {
+			while (i < de) {
 				bitMod = i <= animBits ? 1 - ((1 - minBitMod) / animBits * i) : bitMod = minBitMod;
-				angle = 270 - 360 / this.density * i;
+				ang = 270 - 360 / de * i;
 				switch (this.shape) {
 				case shapes[1]:
 					w = h = size * bitMod;
@@ -519,7 +519,7 @@
 					arc = this.shape === shapes[4] ? 0.6 : 0; 
 					break;
 				}
-				g = setAttr(setCSS(addEl("group", this.vml), {width: d, height: d, rotation: angle}), {coordsize: d + "," + d, coordorigin: -d * 0.5 + "," + (-d * 0.5)});
+				g = setAttr(setCSS(addEl("group", this.vml), {width: d, height: d, rotation: ang}), {coordsize: d + "," + d, coordorigin: -d * 0.5 + "," + (-d * 0.5)});
 				s = setCSS(addEl(sh, g, {stroked: false, arcSize: arc}), { width: w, height: h, top: y, left: x});
 				f = addEl("fill", s, {color: this.color, opacity: bitMod});
 				++i;
@@ -534,11 +534,12 @@
 	*/
 	p.clean = function () {
 		if (engine === engines[0]) {
-			this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+			this.con.clearRect(0, 0, 10000, 10000);
 		} else {
-			if (this.vml.hasChildNodes()) {
-				while (this.vml.childNodes.length >= 1) {
-					this.vml.removeChild(this.vml.firstChild);
+			var v = this.vml;
+			if (v.hasChildNodes()) {
+				while (v.childNodes.length >= 1) {
+					v.removeChild(v.firstChild);
 				}
 			}
 		}
@@ -570,18 +571,18 @@
 	* @method tick
 	* @protected
 	*/
-	p.tick = function (initialize) {
-		var rotUnit = this.density > 360 ? this.density / 360 : 360 / this.density;
+	p.tick = function (init) {
+		var d = this.density, rotUnit = d > 360 ? d / 360 : 360 / d, c = this.con, di = this.diameter;
 		rotUnit *= this.speed;
-		if (!initialize) { this.activeId += rotUnit; }
+		if (!init) { this.activeId += rotUnit; }
 		if (this.activeId >= 360) { this.activeId -= 360; }
 		if (engine === engines[0]) {
-			this.context.clearRect(0, 0, this.diameter, this.diameter);
-			transformContext(this.context, this.diameter * 0.5, this.diameter * 0.5, Math.PI / 180 * this.activeId);
-			this.context.drawImage(this.cacheCanvas, 0, 0, this.diameter, this.diameter);
-			this.context.restore();
+			c.clearRect(0, 0, di, di);
+			transCon(c, di * 0.5, di * 0.5, Math.PI / 180 * this.activeId);
+			c.drawImage(this.cCan, 0, 0, di, di);
+			c.restore();
 		} else {
-			this.vml.style.rotation = this.activeId;
+			setCSS(this.vml, {rotation:this.activeId});
 		}
 	};
 	/**
@@ -614,12 +615,13 @@
 	* @public
 	*/
 	p.kill = function () {
+		var c = this.cont;
 		if (typeof (this.timer) === "number") { this.hide(); }
 		if (engine === engines[0]) {
-			this.cont.removeChild(this.canvas);
-			this.cont.removeChild(this.cacheCanvas);
+			c.removeChild(this.can);
+			c.removeChild(this.cCan);
 		} else {
-			this.cont.removeChild(this.vml);
+			c.removeChild(this.vml);
 		}
 		var n;
 		for (n in this) { delete this[n]; }
