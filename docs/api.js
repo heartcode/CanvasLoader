a1da758fe949fdcb0188d4e12b85bb44c968dd80
Robert Pataki <heartcode@robertpataki.com>
2013-12-19 13:12:57 +0000

    Add documentation and build contents to the repo

---
+++ docs/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "CanvasLoader"
+    ],
+    "modules": [
+        "CanvasLoader"
+    ],
+    "allModules": [
+        {
+            "displayName": "CanvasLoader",
+            "name": "CanvasLoader",
+            "description": "CanvasLoader uses the HTML5 canvas element in modern browsers and VML in IE6/7/8 to create and animate the most popular preloader shapes (oval, spiral, rectangle, square and rounded rectangle).<br/><br/>\nIt is important to note that CanvasLoader doesn't show up and starts rendering automatically on instantiation. To start rendering and display the loader use the <code>show()</code> method."
+        }
+    ]
+} };
+});
